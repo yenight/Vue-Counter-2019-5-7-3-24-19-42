c3e8bd3717d5ff91b170aa83f1b8ab40b5ffe6fa
williamye <william.ye@oocl.com>
2019-07-19 14:15:56 +0800

    update code

---
+++ src/components/calculate.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="message++">+</button>
+        <p>{{message}}</p>
+        <button @click="sub()">-</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "hello",
+        data: function() {
+            return {
+                message: 0
+            };
+        },
+        methods: {
+            sub: function () {
+               this.message--;
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
